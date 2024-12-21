a741151ed5d40a7287d51a2ee2b5a3401ab5d9ce
A <acs0259@auburn.edu>
2024-12-21 15:05:07 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-import time
+import os
 
 pygame.init()
 
@@ -41,31 +41,55 @@ def main():
 class Groob(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        #not finsihed
+        self.animations = {
+            "idle": -1,
+            "walking": self.load_animation_frames("C:\\Users\\alexc\\Downloads\\groob art\\walking ani"),
+            "jumping": -1,
+            "hurt": -1,
+        }
+        
         self.img = pygame.image.load("C:\\Users\\alexc\\Downloads\\Groob_img\\Untitled_Artwork 1.png").convert_alpha()
         self.img = pygame.transform.scale(self.img, (25, 25))
         self.rect = self.img.get_frect()
         self.x_velo = 0
         self.y_velo = 0
         self.onGround = False
+        self.state = 'idle'
         
+        
+    def load_animation_frames(self, folder_path):
+        frames = []
+        for file_name in sorted(os.listdir(folder_path)):
+            if file_name.endswith(".png"):  # Adjust for your file extension
+                frame = pygame.image.load(os.path.join(folder_path, file_name)).convert_alpha()
+                frame = pygame.transform.scale(frame, (25, 25))  # Adjust size
+                frames.append(frame)
+        return frames
 
     def update(self, keys, gravity, window_width, window_height):
         
-        self.y_velo = gravity
-
-
+        
+        
+        
+        # gravity
+        self.rect.y += self.y_velo
+        
+        if not self.onGround:
+            self.y_velo += gravity
+        #move l/r
         if keys[pygame.K_a]:
             self.rect.x -= 5
         if keys[pygame.K_d]:
             self.rect.x += 5
         
         #jump
-        if keys[pygame.K_SPACE] and self.onGround == True:
+        if keys[pygame.K_SPACE] and self.onGround:
             
-                
+            self.y_velo -= 25
             self.onGround = False
            
-        self.rect.y += self.y_velo
+        
 
         #collisions
         if self.rect.bottom >= window_height:
